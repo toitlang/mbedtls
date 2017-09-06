9646609c2b60649810fa01f4e39e8734f90a9076
Angus Gratton <angus@espressif.com>
2017-09-06 15:06:57 +1000

    mbedtls: Re-apply MBEDTLS_BIGNUM_ALT & related macros for custom bignum functions
    (IDF-specific patch.)
    
    (cherry picked from commit f859b9bc7579246703446c158270ae038442351c)

--- library/bignum.c
+++ library/bignum.c
@@ -49,6 +49,8 @@
 
 #include "mbedtls/platform.h"
 
+#if !defined(MBEDTLS_BIGNUM_ALT)
+
 #define MPI_VALIDATE_RET(cond)                                       \
     MBEDTLS_INTERNAL_VALIDATE_RET(cond, MBEDTLS_ERR_MPI_BAD_INPUT_DATA)
 #define MPI_VALIDATE(cond)                                           \
@@ -1243,6 +1245,7 @@ int mbedtls_mpi_sub_int(mbedtls_mpi *X, const mbedtls_mpi *A, mbedtls_mpi_sint b
     return mbedtls_mpi_sub_mpi(X, A, &B);
 }
 
+#if !defined(MBEDTLS_MPI_MUL_MPI_ALT)
 /*
  * Baseline multiplication: X = A * B  (HAC 14.12)
  */
@@ -1344,6 +1347,7 @@ int mbedtls_mpi_mul_int(mbedtls_mpi *X, const mbedtls_mpi *A, mbedtls_mpi_uint b
 cleanup:
     return ret;
 }
+#endif
 
 /*
  * Unsigned integer divide - double mbedtls_mpi_uint dividend, u1/u0, and
@@ -1680,6 +1684,8 @@ int mbedtls_mpi_mod_int(mbedtls_mpi_uint *r, const mbedtls_mpi *A, mbedtls_mpi_s
 
     return 0;
 }
+
+#if !defined(MBEDTLS_MPI_EXP_MOD_ALT)
 
 static void mpi_montg_init(mbedtls_mpi_uint *mm, const mbedtls_mpi *N)
 {
@@ -2055,6 +2061,7 @@ cleanup:
 
     return ret;
 }
+#endif
 
 /*
  * Greatest common divisor: G = gcd(A, B)  (HAC 14.54)
@@ -2629,6 +2636,7 @@ cleanup:
 }
 
 #endif /* MBEDTLS_GENPRIME */
+#endif /* MBEDTLS_BIGNUM_ALT */
 
 #if defined(MBEDTLS_SELF_TEST)
 
